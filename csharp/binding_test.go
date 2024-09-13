495b7733796e196d1fad87cad1f817154708d4a0
lihua.llh <lihua.llh@alibaba-inc.com>
2024-09-13 17:40:02 +0800

    udpate csharp to 0.23.0

--- csharp/binding_test.go
+++ csharp/binding_test.go
@@ -19,3 +19,23 @@ func TestGrammar(t *testing.T) {
 		n.String(),
 	)
 }
+
+func TestGrammar_Complex(t *testing.T) {
+	assert := assert.New(t)
+
+	n, err := sitter.ParseCtx(context.Background(), []byte(`public class OrderServices(
+    IMediator mediator,
+    IOrderQueries queries,
+    IIdentityService identityService,
+    ILogger<OrderServices> logger) 
+	: IOrderQueries
+{
+    public IMediator Mediator { get; set; } = mediator;
+    public ILogger<OrderServices> Logger { get; } = logger;
+}`), csharp.GetLanguage())
+	assert.NoError(err)
+	assert.Equal(
+		"(compilation_unit (class_declaration (modifier) name: (identifier) (parameter_list (parameter type: (identifier) name: (identifier)) (parameter type: (identifier) name: (identifier)) (parameter type: (identifier) name: (identifier)) (parameter type: (generic_name (identifier) (type_argument_list (identifier))) name: (identifier))) (base_list (identifier)) body: (declaration_list (property_declaration (modifier) type: (identifier) name: (identifier) accessors: (accessor_list (accessor_declaration) (accessor_declaration)) value: (identifier)) (property_declaration (modifier) type: (generic_name (identifier) (type_argument_list (identifier))) name: (identifier) accessors: (accessor_list (accessor_declaration)) value: (identifier)))))",
+		n.String(),
+	)
+}
